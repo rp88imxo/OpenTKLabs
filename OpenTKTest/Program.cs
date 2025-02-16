35f04dc5616f9f9403d49a407ab4ab7b0c0aeacf
rp88imxo <rp88imxo@gmail.com>
2025-02-16 19:41:19 +0300

    lab task

--- OpenTKTest/Program.cs
+++ OpenTKTest/Program.cs
@@ -8,32 +8,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        float[] vertices =
-        {
-            -0.5f, -0.5f, 0.0f, //Bottom-left vertex
-            0.5f, -0.5f, 0.0f, //Bottom-right vertex
-            0.0f, 0.5f, 0.0f //Top vertex
-        };
-        
-        float[] vertices2 =
-        {
-            0.5f,  0.5f, 0.0f,  // top right
-            0.5f, -0.5f, 0.0f,  // bottom right
-            -0.5f, -0.5f, 0.0f,  // bottom left
-            -0.5f,  0.5f, 0.0f   // top left
-        };
-        
-        uint[] indices = {  // note that we start from 0!
-            0, 1, 3,   // first triangle
-            1, 2, 3    // second triangle
-        };
-
         using (var baseWindow = new BaseWindow(800, 600, "OpenGL", new List<IRenderOperation>()
                {
-                   new TriangleRenderOperation(vertices,
+                   new TriangleRenderOperation(TrianglesData.VerticesTriangleRed,
                        new DefaultShader("Shaders/vertex.shader", "Shaders/fragment.shader")),
-                   new ElementBufferRenderOperation(vertices2,indices,
-                       new DefaultShader("Shaders/vertex.shader", "Shaders/fragment.shader")),
+                   new TriangleRenderOperation(TrianglesData.VerticesTriangleGreen,
                }))
         {
             baseWindow.Run();
---
+++ OpenTKTest/TrianglesData.cs
@@ -0,0 +1,25 @@
+namespace OpenTKTest;
+
+public static class TrianglesData
+{
+    public static  float[] VerticesTriangleRed =
+    {
+        -0.5f, -0.5f, 0.0f,
+        0.5f, -0.5f, 0.0f, 
+        0.0f, 0.5f, 0.0f
+    };
+        
+    public static  float[] VerticesTriangleGreen =
+    {
+        -0.3f, -0.3f, 0.0f,
+        0.3f, -0.3f, 0.0f, 
+        0.0f, 0.3f, 0.0f
+    };
+    
+    public static  float[] VerticesTriangleBlue =
+    {
+        -0.1f, -0.1f, 0.0f,
+        0.1f, -0.1f, 0.0f, 
+        0.0f, 0.1f, 0.0f
+    };
+}
